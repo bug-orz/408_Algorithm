79923eabd388f82fadf356a8c1739bc6c32c435e
bug-orz <bohr.y@outlook.com>
2021-12-01 18:45:57 +0800

    完成2009年算法题

--- code/2009/2009/2009/2009.cpp
+++ code/2009/2009/2009/2009.cpp
@@ -1,20 +1,61 @@
-﻿// 2009.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+struct node {
+    double data;
+    node* link;
+};
+
+class linked_list {
+    node* list;
+    
+public:
+    linked_list(int len = 50);
+    double find_k(int k=5);
+    void test();
+};
+
+linked_list::linked_list(int len) {
+    node* temp;
+    list = new node();
+    list->data = 0.0;
+    temp = list;
+    for (int i = 1; i <= len; i++) {
+        node* p = new node();
+        p->data = i;
+        temp->link = p;
+        temp = temp->link;
+    }
+}
+
+double linked_list::find_k(int k) {
+    node* p1, *p2;
+    p1 = list;
+    p2 = list;
+    for (int i = 0; i < k; i++) {
+        p2 = p2->link;
+    }
+    while (p2) {
+        p1 = p1->link;
+        p2 = p2->link;
+    }
+    return p1->data;
+}
+
+void linked_list::test() {
+    cout << list->data;
+    node* p = list;
+    for (int i = 0; i < 10; i++) {
+        cout << p->data;
+        p = p->link;
+    }
+    
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //linked_list().test();
+    double ans = linked_list().find_k();
+    cout << ans;
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
