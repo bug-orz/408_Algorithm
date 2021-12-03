363230ce61961054d5867d6598c079cacdb03c1a
bug-orz <bohr.y@outlook.com>
2021-12-03 19:38:29 +0800

    新增并查集，以及使用并查集寻找环和连通分量。新增邻接表

---
+++ code/Disjoint Sets 并查集/Disjoint Sets 并查集/Disjoint Sets 并查集.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+using namespace std;
+#define NUM_of_NODES 4
+
+class Disjoint_Sets {
+    int set[NUM_of_NODES];
+public:
+    Disjoint_Sets();
+    int find(int x);
+    void union_(int i, int j);
+};
+
+Disjoint_Sets::Disjoint_Sets() {
+    for (int i = 0; i < NUM_of_NODES; i++) {
+        set[i] = -1;
+    }
+}
+
+int Disjoint_Sets::find(int x) {
+    int temp = x;
+    while (set[temp] >= 0) {
+        temp = set[temp];
+    }
+    int head = temp;
+    temp = x;
+    while (set[temp] >= 0) {
+        set[temp] = head;
+        temp = set[temp];
+    }
+    return temp;
+}
+
+void Disjoint_Sets::union_(int i, int j) {
+    i = find(i);
+    j = find(j);
+    if (i == j) {
+        return;
+    }
+    if (i > j) {
+        set[j] += set[i];
+        set[i] = j;
+    }
+    else {
+        set[i] += set[j];
+        set[j] = i;
+    }
+}
+bool check_circle(int g[4][4]) { //寻找图中的环
+    Disjoint_Sets d;
+    for (int i = 0; i < 4; i++) {
+        for (int j = i; j < 4; j++) {
+            if (g[i][j] != 0) {
+                if (d.find(i) == d.find(j)) {
+                    return true;
+                }
+                else {
+                    d.union_(i, j);
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
+int main()
+{
+    int graph1[4][4] = { { 0,1,1,0 },{1,0,0,1},{1,0,0,1},{0,1,1,0} };
+    int graph2[4][4] = { { 0,1,1,0 },{1,0,0,1},{1,0,0,0},{0,1,0,0} };
+    cout << check_circle(graph1) << endl;
+    cout << check_circle(graph2) << endl;
+    return 0;
+}
---
+++ code/邻接表/邻接表/邻接表.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+#define N 3
+
+struct ArcNode {
+    int adjvex;
+    ArcNode* next;
+};
+
+struct Node {
+    int data;
+    ArcNode* first;
+};
+
+int main()
+{
+    Node adjList[N];
+    ArcNode* n1 = new ArcNode();
+    n1->adjvex = 1;
+    ArcNode * n2 = new ArcNode();
+    n1->adjvex = 2;
+    ArcNode* n3 = new ArcNode();
+    n1->adjvex = 3;
+    adjList[0].first = n1;
+    n1->next = n2;
+    cout << "Hello World!\n";
+}
